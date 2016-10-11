6d02428d36fd84b325b583e2dda02c201e68f73c
Philipp Muens <raute1337@gmx.de>
2016-10-11 13:49:50 -0700

    Update todos-update.js

--- todos-update.js
+++ todos-update.js
@@ -17,8 +17,7 @@ module.exports = (event, callback) => {
   return dynamoDb.put(params, (error, data) => {
     if (error) {
       callback(error);
-    } else {
-      callback(error, params.Item);
     }
+    callback(error, params.Item);
   });
 };
